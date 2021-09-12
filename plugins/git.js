744fe1c1a12c75377b854e903247072402f90205
Yasir Sial <90135521+Yasirsial@users.noreply.github.com>
2021-09-12 15:14:01 +0500

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,7 +11,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "https://ibb.co/LrTm0Qs";
+  r_text[0] = "https://www.pixelstalk.net/wp-content/uploads/2016/06/Thor-Backgrounds-For-Desktop.jpg";
     
     
     var i = Math.floor(1*Math.random())
